45a1d54e3f7774c8d57efdc43af6819a4a47e99f
wangguan <wangguan09@kuaishou.com>
2023-11-01 18:29:51 +0800

    modify meta

--- components/Meta.tsx
+++ components/Meta.tsx
@@ -1,26 +1,27 @@
 import Head from "next/head";
 import React from "react";
 
-export const Meta = ({ title, description, url }) => {
-  return (
-    <Head>
-      <title>{title}</title>
-      <link rel="icon" href="/static/favicon.png" type="image/png" />
-      <meta content={description} name="description" />
-      <meta name="og:url" content={url} />
-      <meta name="og:title" content={title} />
-      <meta name="og:description" content={description} />
-      <meta name="og:image" content={"https://transform.tools/cover.png"} />
-      <meta name="og:type" content="website" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta
-        name="twitter:image"
-        content={"https://transform.tools/cover.png"}
-      />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content="ritz078" />
-      <link rel="manifest" href="/static/site.webmanifest" />
-    </Head>
-  );
+export const Meta = ({title, description, url}) => {
+    return (
+        <Head>
+            <title>{title}</title>
+            <link rel="icon" href="/static/favicon.png" type="image/png"/>
+            <meta content={description} name="description"/>
+            <link rel="canonical" href={url}/>
+            <meta name="og:url" content={url}/>
+            <meta name="og:title" content={title}/>
+            <meta name="og:description" content={description}/>
+            <meta name="og:image" content={"https://translatorl.com/cover.png"}/>
+            <meta name="og:type" content="website"/>
+            <meta name="twitter:title" content={title}/>
+            <meta name="twitter:description" content={description}/>
+            <meta
+                name="twitter:image"
+                content={"https://translatorl.com/cover.png"}
+            />
+            <meta name="twitter:card" content="summary_large_image"/>
+            <meta name="twitter:creator" content="ritz078"/>
+            <link rel="manifest" href="/static/site.webmanifest"/>
+        </Head>
+    );
 };
